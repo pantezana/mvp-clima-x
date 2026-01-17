9a7e9c5b987264147bef543cc9119ed42538a3e7
pantezana <pantezana@grupoanteru.com>
2026-01-16 19:15:15 -0500

    Create app.py
    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="MVP Clima en X", layout="wide")
+
+st.title("📊 MVP – Clima del Tema en X")
+st.write("App desplegada correctamente desde GitHub + Streamlit Cloud")
+
+st.success("Si ves esto, el deploy funciona ✅")
