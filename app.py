3ba790df841b0d402e28d6eea5975b8c76878462
pantezana <pantezana@grupoanteru.com>
2026-01-16 22:49:24 -0500

    Update 10 app.py
    Update 10 app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import streamlit as st
+import tweepy
 import re
 import pandas as pd
 from datetime import datetime, timedelta
+
+st.set_page_config(page_title="MVP Clima en X", layout="wide")
+st.title("📊 MVP – Clima del Tema en X")
+
+bearer_token = st.secrets["X_BEARER_TOKEN"]
+client = tweepy.Client(bearer_token=bearer_token)
+
+st.success("Conectado a X correctamente ✅")
+
+query = st.text_input("Palabras clave / hashtags")
+
+time_range = st.selectbox(
+    "Rango temporal",
+    ["24 horas", "7 días", "30 días"]
+)
+
 
 # Lista simple (MVP) de departamentos/ciudades clave para inferir ubicación
 PERU_PLACES = [
